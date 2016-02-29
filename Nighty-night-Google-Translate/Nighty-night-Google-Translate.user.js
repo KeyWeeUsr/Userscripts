91986e17207c740bb85f58d80159730fe714d5dc
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2016-02-29 22:29:12 +0100

    Add notifications, user panel in Translate

--- Nighty-night-Google-Translate/Nighty-night-Google-Translate.user.js
+++ Nighty-night-Google-Translate/Nighty-night-Google-Translate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nighty-night Google Translate
 // @namespace    https://github.com/KeyWeeUsr/Userscripts
-// @version      0.2
+// @version      0.3
 // @description  Translate at night comfortably!
 // @author       Peter Badida
 // @copyright    2016+, Peter Badida
@@ -9,6 +9,7 @@
 // @homepage     https://github.com/KeyWeeUsr/Userscripts/tree/master/Nighty-night-Google-Translate
 // @supportURL   https://github.com/KeyWeeUsr/Userscripts/issues
 // @include      https://*translate.google.*/*
+// @include      https://plus.google.*/u/0/_/notifications/frame*
 // @grant        GM_addStyle
 // ==/UserScript==
 /* jshint -W097 */
@@ -17,32 +18,41 @@
 (function () {
 var css = "\
 /*Text,Buttons,Inputs*/\
-div.gt-ex-text, span.gt-syn-span, div.goog-toolbar-button, .goog-toolbar-menu-button {color: #777777;}\
+div.gb_lb, div.gt-ex-text, span.gt-syn-span, div.goog-toolbar-button,\
+.goog-toolbar-menu-button {color: #777777;}\
 li.ita-kd-menuitem, span.ita-kd-menuitem-inputtool-name, div.goog-menuitem-content, span.gt-card-ttl-txt,\
 span.gt-rw-span, div.gt-def-row, a, div.gt-baf-word-clickable, textarea#source {color: #777777 !important;}\
-button.jfk-button-action, button.vk-btn, span.ita-kd-inputtools-div, div.goog-flat-menu-button,\
-input.jfk-button-action, div.jfk-button-standard{color: #444444; background-image: none; background-color:\
-#696969 !important; border-color: #696969;}\
+div#gbqfqw, input#gbqfq, input#gt-pb-sb, button.jfk-button-action, button.vk-btn,\
+span.ita-kd-inputtools-div, div.goog-flat-menu-button, input.jfk-button-action, div.jfk-button-standard,\
+a.gb_Ca{color: #444444; background-image: none; background-color: #696969 !important; border-color: #696969;}\
+button.gbqfb, a.gb_nb {background:#696969 !important; border-color: #666666 !important;\
+color: #444444 !important;}\
+button.gbqfb:hover, a.gb_nb:hover {background-color: #f8f8f8 !important;}\
 \
 /*Borders*/\
-div#gt-src-wrap {border-color: #777777 !important;}\
+div#gbsfw, div#gt-src-wrap {border-color: #777777 !important;}\
 div#pb-tb-c, span#result_box, div#gt-res-wrap {border-color: #777777; color: #777777;background: transparent\
  !important;}\
 div.cd-exp-ar:after {border-top: 16px solid #222222;}\
 div.cd-exp-ar:before {border-top:16px solid #696969;}\
+div.gb_ab {border-bottom-color: #333333 !important;}\
+div.Kza:after, div.Kza:before {border-top-color: #666666;}\
 \
 /*Backgrounds*/\
-div.gb_R {background-color: #555555 !important;}\
+select#gt-sl, select#gt-tl, div.Kza, div.gb_R {background-color: #555555 !important;\
+background: #555555 !important;}\
+select#gt-sl:hover, select#gt-tl:hover {background:#f8f8f8 !important;}\
 div#gt-ft-res, div.gb_Jb, div.gb_T {background-color: black !important; border: 0;}\
 div#pb-tool, div#gt-apb-main, div#gt-appbar {background-color: black !important; background: black !important;\
  border: 0;}\
-div#gt-text-c, div#gt-text-top, body, div#gt-lc {background-color: #222222 !important; background: #222222\
- !important;}\
-div#pb-st-menu,div#pb-ls-menu,ul.ita-kd-dropdown-menu, div#gt-sl-gms-menu, div#gt-tl-gms-menu, div.vk-box\
- {background-color: #333333;}\
+div.MNn0h, a.gb_ka, div.gb_ob, div#gt-text-c, div#gt-text-top, body, div#gt-lc\
+{background-color: #222222 !important; background: #222222 !important;}\
+div#pb-st-menu,div#pb-ls-menu,ul.ita-kd-dropdown-menu, div#gt-sl-gms-menu, div#gt-tl-gms-menu, div.vk-box,\
+div.aac, ul.gb_ja, ul.gb_ca, div.gb_ga {background-color: #333333 !important;}\
 div.cd-exp-ar {background-color: #696969;}\
 \
 /*Miscellaneous*/\
+div.contentframe {color: inherit; background: 0; background-color: transparent;}\
 div#t-new-user {display: none;}\
 span.gt-cd-cl {text-decoration: underline;}\
 a.gb_Lb {text-decoration: none; font-size: 250%;}\
@@ -54,7 +64,5 @@ div.gb_Jb {padding: 0 20px 0 24px; width: 80%;}\
 "
 GM_addStyle(css);})();
 /*
-Missing:
-1) Notifications
-2) User panel
+Notify me if there's something missing/undesirable.
 */
