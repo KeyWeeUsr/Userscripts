c9b3e5889819aa6b894648ea81c0e3ae0e7253fc
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2016-04-16 10:52:17 +0200

    Fix translations color

--- Nighty-night-Google-Translate/Nighty-night-Google-Translate.user.js
+++ Nighty-night-Google-Translate/Nighty-night-Google-Translate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nighty-night Google Translate
 // @namespace    https://github.com/KeyWeeUsr/Userscripts
-// @version      0.8
+// @version      0.9
 // @description  Translate at night comfortably!
 // @author       Peter Badida
 // @copyright    2016+, Peter Badida
@@ -17,9 +17,32 @@
 
 (function () {
 var css = "\
+/*TOOLBAR*/\
+div#gt-apb-main, div#gt-appbar {\
+    background-color: black !important;\
+    background: black !important;\
+    border: 0;\
+}\
+span.gb_Pb {\
+    background-image: none;\
+    display: inline !important;\
+    left:-10px;\
+}\
+span.gb_Pb:after {\
+    content: 'Google';\
+}\
+a.gb_Nb {\
+    text-decoration: none;\
+    font-size: 250%;\
+}\
+div.gb_Td>.gb_R {\
+    line-height: 60px;\
+}\
+\
 /*TEXT*/\
-div#spelling-correction, div.gb_nb, div.gt-ex-text, span.gt-syn-span, div.goog-toolbar-button,\
-.goog-toolbar-menu-button, div.gt-revert-correct-message {\
+div#spelling-correction, div.gb_nb, div.gt-ex-text, span.gt-syn-span,\
+div.goog-toolbar-button, .goog-toolbar-menu-button,\
+div.gt-revert-correct-message {\
     color: #777777;\
 }\
 a, textarea#source {\
@@ -30,10 +53,17 @@ a, textarea#source {\
 span.gt-card-ttl-txt, div.gt-def-row, span.gt-rw-span {\
     color: #777777 !important;\
 }\
+span.gt-cd-cl {\
+    text-decoration: underline;\
+}\
 \
 /*TEXT TRANSLATIONS*/\
-div.gt-baf-word-clickable {\
+span.gt-baf-word-clickable {\
     color: #777777 !important;\
+}\
+\
+span.gt-baf-hl {\
+    color: #f1f1f1 !important;\
 }\
 \
 /*MENU*/\
@@ -46,6 +76,16 @@ div.jfk-button-standard, div.goog-flat-menu-button {\
 div.goog-menuitem-content {\
     color: #777777 !important;\
 }\
+div.goog-menuitem-highlight {\
+    background-color: #f1f1f1 !important;\
+    border-color: #f1f1f1 !important;\
+}\
+div.goog-menuitem-emphasize-highlight {\
+    background-color: #595959 !important;\
+}\
+div.goog-option-selected {\
+    background-color: #595959 !important;\
+}\
 input#gt-submit:hover {\
     background-color: #f1f1f1 !important;\
     background-image: none !important;\
@@ -53,6 +93,36 @@ input#gt-submit:hover {\
 div.jfk-button-checked, div.goog-flat-menu-button-hover, div.goog-flat-menu-button-open {\
     background-color: #f1f1f1 !important;\
     background-image: none !important;\
+}\
+div#gt-sl-gms-menu, div#gt-tl-gms-menu {\
+    background-color: #333333 !important;\
+}\
+\
+/*BACKGROUNDS*/\
+div.gb_Lb, div#gt-ft-res {\
+    background-color: black !important;\
+    border: 0;\
+}\
+body, div#gt-text-c, div#gt-text-top, div#gt-lc {\
+    background-color: #222222 !important;\
+    background: #222222 !important;\
+}\
+select#gt-sl, select#gt-tl {\
+    background-color: #555555 !important;\
+    background: #555555 !important;\
+}\
+select#gt-sl:hover, select#gt-tl:hover {\
+    background:#f8f8f8 !important;\
+}\
+\
+/*BORDERS*/\
+div#gt-src-wrap {\
+    border-color: #777777 !important;\
+}\
+span#result_box, div#gt-res-wrap {\
+    border-color: #777777;\
+    color: #777777;\
+    background: transparent !important;\
 }\
 \
 /*BLUE LINKS BUTTONS*/\
@@ -89,13 +159,34 @@ div.pbdel-button:hover, button#gt-pb-sbt:hover {\
     background-color: #f1f1f1 !important;\
     background-image: none !important;\
 }\
+div#pb-tool {\
+    background-color: black !important;\
+    background: black !important;\
+    border: 0;\
+}\
+div#pb-st-menu, div#pb-ls-menu {\
+    background-color: #333333 !important;\
+}\
+tr.nolabel {\
+    background-color: #555555 !important;\
+    background: #555555 !important;\
+    border-top: 1px solid #777777 !important;\
+}\
+div#pb-sp-del, #gt-pb-tb, #gt-pb-tb tr {\
+    border: 0 !important;\
+}\
+div#pb-tb-c{\
+    border-color: #777777;\
+    color: #777777;\
+    background: transparent !important;\
+}\
 \
 /*INPUTTOOLS*/\
 li.ita-kd-menuitem, span.ita-kd-menuitem-inputtool-name {\
     color: #777777 !important;\
 }\
-div.ita-hwt-candidate, span.ita-kd-inputtools-div, div.ita-hwt-jfk-standard,\
-div.ita-hwt-jfk-action {\
+div.ita-hwt-candidate, span.ita-kd-inputtools-div,\
+div.ita-hwt-jfk-standard, div.ita-hwt-jfk-action {\
     color: #393939; background-image: none !important;\
     background-color: #696969 !important; border-color: #696969;\
 }\
@@ -142,8 +233,20 @@ div.vk-box {\
 ul.gb_ja, ul.gb_ca {\
     background-color: #333333 !important;\
 }\
+a.gb_ka {\
+    background-color: #222222 !important;\
+    background: #222222 !important;\
+}\
+div.MNn0h {\
+    background-color: #222222 !important;\
+    background: #222222 !important;\
+}\
 \
 /*NOTIFICATIONS*/\
+body.Vzc {\
+    background-color: transparent !important;\
+    background: transparent !important;\
+}\
 div#gbsfw {\
     border-color: #777777 !important;\
 }\
@@ -157,6 +260,13 @@ div.gb_qb {\
     background-color: #222222 !important;\
     background: #222222 !important;\
 }\
+div.Kza {\
+    background-color: #555555 !important;\
+    background: #555555 !important;\
+}\
+div.Kza:after, div.Kza:before {\
+    border-top-color: #666666;\
+}\
 \
 /*USERPANEL*/\
 div.gb_R {\
@@ -168,53 +278,49 @@ div.gb_T {\
     border: 0;\
 }\
 \
+/*SHARE*/\
+div.jfk-bubble {\
+    background-color: #393939;\
+    border-color: #777777 !important;\
+}\
+div.share-panel{\
+    background: none;\
+}\
+div.share-panel>h3 {\
+    color: #777777 !important;\
+}\
 \
-\
-/*sort borders & bgs into categories!*/\
-\
-\
-/*Borders*/\
-div#gt-src-wrap {border-color: #777777 !important;}\
-div#pb-tb-c, span#result_box, div#gt-res-wrap {border-color: #777777; color: #777777;\
-background: transparent !important;}\
-div.cd-exp-ar:after {border-top: 16px solid #222222;}\
-div.cd-exp-ar:before {border-top:16px solid #696969;}\
-div.gb_cb {border-bottom-color: #333333 !important;}\
-div.Kza:after, div.Kza:before {border-top-color: #666666;}\
-div#pb-sp-del, #gt-pb-tb, #gt-pb-tb tr {border: 0 !important;}\
-tr.nolabel {border-top: 1px solid #777777 !important;}\
-\
-/*Backgrounds*/\
-select#gt-sl, select#gt-tl, div.Kza, tr.nolabel {background-color: #555555 !important;\
-background: #555555 !important;}\
-select#gt-sl:hover, select#gt-tl:hover {background:#f8f8f8 !important;}\
-div#gt-ft-res, div.gb_Lb {background-color: black !important; border: 0;}\
-div#pb-tool, div#gt-apb-main, div#gt-appbar {background-color: black !important;\
-background: black !important; border: 0;}\
-div.MNn0h, a.gb_ka, div#gt-text-c, div#gt-text-top, body,\
-div#gt-lc {background-color: #222222 !important; background: #222222 !important;}\
-div#pb-st-menu,div#pb-ls-menu, div#gt-sl-gms-menu,\
-div#gt-tl-gms-menu {background-color: #333333 !important;}\
-div.cd-exp-ar {background-color: #696969;}\
+/*ARROWS*/\
+div.cd-exp-ar {\
+    background-color: #696969;\
+}\
+div.cd-exp-ar:after {\
+    border-top: 16px solid #222222;\
+}\
+div.cd-exp-ar:before {\
+    border-top:16px solid #696969;\
+}\
+div.jfk-bubble-arrowimplafter {\
+    border-color: #777777 transparent !important;\
+}\
+div.gb_cb {\
+    border-bottom-color: #333333 !important;\
+}\
 \
 /*Miscellaneous*/\
-div.contentframe {color: inherit; background: 0; background-color: transparent;}\
-div#t-new-user {display: none;}\
-span.gt-cd-cl {text-decoration: underline;}\
-a.gb_Nb {text-decoration: none; font-size: 250%;}\
-span.gb_Pb {background-image: none; display: inline !important; left:-10px;}\
-span.gb_Pb:after {content: 'Google';}\
-div.gb_Td>.gb_R {line-height: 60px;}\
-img#kwu_av:hover {opacity: 1 !important;}\
-body.Vzc {background-color: transparent !important; background: transparent !important;}\
+div.contentframe {\
+    color: inherit;\
+    background: 0;\
+    background-color: transparent;\
+}\
+div#t-new-user {\
+    display: none;\
+}\
+img#kwu_av:hover {\
+    opacity: 1 !important;\
+}\
 \
-\
-/*TRASH?*/\
-a.gb_Ca {color: #393939; background-image: none !important; background-color: #696969 !important; border-color: #696969;}\
-div#gbqfqw, input#gbqfq {color: #393939; background-image: none !important; background-color: #696969 !important; border-color: #696969;}\
-button.gbqfb {background:#696969 !important; border-color: #666666 !important; color: #444444 !important;}\
-button.gbqfb:hover {background-color: #f1f1f1 !important; background-image: none !important; }\
-"
+";
 GM_addStyle(css);
 var panel=document.getElementById('gt-lang-right');
 var av='<a href="https://github.com/KeyWeeUsr/Userscripts"><img id="kwu_av"\
