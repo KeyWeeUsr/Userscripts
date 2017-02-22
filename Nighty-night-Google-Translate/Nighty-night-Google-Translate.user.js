8baae42ccb3ede9b2fee96884d5730725b515125
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-02-22 23:59:09 +0100

    Translate - Move colors to variables

--- Nighty-night-Google-Translate/Nighty-night-Google-Translate.user.js
+++ Nighty-night-Google-Translate/Nighty-night-Google-Translate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nighty-night Google Translate
 // @namespace    https://github.com/KeyWeeUsr/Userscripts
-// @version      1.7
+// @version      1.8
 // @description  Translate at night comfortably!
 // @author       Peter Badida
 // @copyright    2016+, Peter Badida
@@ -18,14 +18,23 @@
 'use strict';
 
 (function () {
+var bg_black = '#111111';
+var bg_dark = '#191919';
+var bg_medium = '#222222';
+var modals = '#333333';
+var text = '#393939';
+var bg_light = '#696969';
+var light = '#777777';
+var highlight = '#F1F1F1';
+
 var css = "\
 /*TOOLBAR*/\
 div#gt-apb-main, div#gt-appbar {\
-    background-color: #191919 !important;\
-    background: #191919 !important;\
+    background-color: {bg_dark} !important;\
+    background: {bg_dark} !important;\
     border: 0;\
 }\
-div#gb > div > div+div > div > a > span ,span.gb_Qb {\
+div#gb > div > div+div > div > a > span, span.gb_Qb {\
     background-image: none;\
     display: inline !important;\
 }\
@@ -46,15 +55,15 @@ div.goog-toolbar-button, .goog-toolbar-menu-button,\
 div.gt-revert-correct-message, span#gt-feedback-caption,\
 div.gt-src-cc-normal, span#gt-ovfl-xlt-more,\
 div#gt-src-is-list > div > div {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 a, textarea#source, .vt-dismiss {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 \
 /*TEXT DEFINITIONS*/\
 span.gt-card-ttl-txt, div.gt-def-row, span.gt-rw-span {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 span.gt-cd-cl {\
     text-decoration: underline;\
@@ -62,80 +71,80 @@ span.gt-cd-cl {\
 \
 /*TEXT TRANSLATIONS*/\
 span.gt-baf-word-clickable {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 \
 span.gt-baf-hl {\
-    color: #f1f1f1 !important;\
+    color: {highlight} !important;\
 }\
 \
 /*MENU*/\
 div.jfk-button-standard, div.jfk-button-standard:focus,\
 div.goog-flat-menu-button {\
-    color: #393939;\
+    color: {text};\
     background-image: none !important;\
-    background-color: #696969 !important;\
-    border-color: #696969;\
+    background-color: {bg_light} !important;\
+    border-color: {bg_light};\
 }\
 div.goog-menuitem-content {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 div.goog-menuitem-highlight,\
 div#gt-sl-gms-menu .goog-menuitem-group .goog-menuitem-highlight,\
 div#gt-tl-gms-menu .goog-menuitem-group .goog-menuitem-highlight {\
-    background-color: #222222 !important;\
-    border-color: #222222 !important;\
+    background-color: {bg_medium} !important;\
+    border-color: {bg_medium} !important;\
 }\
 div.goog-menuitem-emphasize-highlight {\
-    background-color: #222222 !important;\
+    background-color: {bg_medium} !important;\
 }\
 div.goog-option-selected {\
-    background-color: #222222 !important;\
+    background-color: {bg_medium} !important;\
 }\
 input#gt-submit:hover {\
-    background-color: #f1f1f1 !important;\
+    background-color: {highlight} !important;\
     background-image: none !important;\
 }\
 div.jfk-button-checked, div.goog-flat-menu-button-hover,\
 div.goog-flat-menu-button-open {\
-    background-color: #f1f1f1 !important;\
+    background-color: {highlight} !important;\
     background-image: none !important;\
 }\
 div#gt-sl-gms-menu, div#gt-tl-gms-menu {\
-    background-color: #333333 !important;\
+    background-color: {text} !important;\
 }\
 \
 /*BACKGROUNDS*/\
 div#gt-ft-res {\
-    background-color: #111111 !important;\
+    background-color: {bg_black} !important;\
     border: 0;\
 }\
 body, div#gt-text-c, div#gt-text-top, div#gt-lc {\
-    background-color: #222222 !important;\
-    background: #222222 !important;\
+    background-color: {bg_medium} !important;\
+    background: {bg_medium} !important;\
 }\
 select#gt-sl, select#gt-tl {\
-    background-color: #555555 !important;\
-    background: #555555 !important;\
+    background-color: {text} !important;\
+    background: {text} !important;\
 }\
 select#gt-sl:hover, select#gt-tl:hover {\
-    background:#f8f8f8 !important;\
+    background: {highlight} !important;\
 }\
 div.gt-is, div#gt-ovfl {\
-    background-color: #333333;\
+    background-color: {modals};\
     border-style: none;\
 }\
 div.gt-is-itm-hover {\
-    background-color: #222222;\
+    background-color: {bg_medium};\
 }\
 \
 /*BORDERS*/\
 div#gt-src-wrap, div.goog-menuseparator, div.goog-toolbar-separator {\
-    border-color: #777777 !important;\
+    border-color: {light} !important;\
 }\
 span#result_box, div#gt-res-wrap {\
-    border-color: #777777;\
-    color: #777777;\
+    border-color: {light};\
+    color: {light};\
     background: transparent !important;\
 }\
 .goog-toolbar-menu-button-focused, .goog-flat-menu-button-focused {\
@@ -144,136 +153,136 @@ span#result_box, div#gt-res-wrap {\
 \
 /*BLUE LINKS BUTTONS*/\
 button.jfk-button-action, input.jfk-button-action, div.jfk-button-action {\
-    color: #393939;\
+    color: {text};\
     background-image: none !important;\
-    background-color: #696969 !important;\
-    border-color: #696969;\
+    background-color: {bg_light} !important;\
+    border-color: {bg_light};\
 }\
 div.jfk-button-hover {\
-    color: #393939;\
-    background-color: #f1f1f1 !important;\
+    color: {text};\
+    background-color: {highlight} !important;\
     background-image: none !important;\
-    border-color: #696969;\
+    border-color: {bg_light};\
 }\
 \
 /*PHRASEBOOK*/\
 input#gt-pb-sb, div.pbdel-button {\
-    color: #393939;\
+    color: {text};\
     background-image: none !important;\
-    background-color: #696969 !important;\
-    border-color: #696969;\
+    background-color: {bg_light} !important;\
+    border-color: {bg_light};\
 }\
 div.pbdel-button:hover, button#gt-pb-sbt:hover {\
-    background-color: #f1f1f1 !important;\
+    background-color: {highlight} !important;\
     background-image: none !important;\
 }\
 div#pb-tool {\
-    background-color: #252525 !important;\
-    background: #252525 !important;\
+    background-color: {bg_medium} !important;\
+    background: {bg_medium} !important;\
     border: 0;\
 }\
 div#pb-st-menu, div#pb-ls-menu {\
-    background-color: #333333 !important;\
+    background-color: {modals} !important;\
 }\
 tr.nolabel {\
-    background-color: #333333 !important;\
-    background: #333333 !important;\
-    border-top: 1px solid #777777 !important;\
+    background-color: {modals} !important;\
+    background: {modals} !important;\
+    border-top: 1px solid {light} !important;\
 }\
 div#pb-sp-del, #gt-pb-tb, #gt-pb-tb tr {\
     border: 0 !important;\
 }\
 div#pb-tb-c, table#gt-pb-tb{\
-    border-color: #777777;\
-    color: #777777 !important;\
+    border-color: {light};\
+    color: {light} !important;\
     background: transparent !important;\
 }\
 \
 /*INPUTTOOLS*/\
 li.ita-kd-menuitem, span.ita-kd-menuitem-inputtool-name {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 span.ita-kd-inputtools-div {\
-    color: #393939; background-image: none !important;\
+    color: {text}; background-image: none !important;\
     background-color: transparent !important;\
-    border-color: #696969;\
+    border-color: {bg_light};\
 }\
 div.ita-hwt-candidate, div.ita-hwt-backspace,\
 div.ita-hwt-jfk-standard, div.ita-hwt-jfk-action {\
-    color: #393939; background-image: none !important;\
-    background-color: #777777 !important;\
-    border-color: #696969;\
+    color: {text}; background-image: none !important;\
+    background-color: {light} !important;\
+    border-color: {bg_light};\
 }\
 div.ita-hwt-candidate-hover, div.ita-hwt-jfk-hover,\
 div.ita-hwt-backspace:hover {\
-    background-color: #f1f1f1 !important;\
+    background-color: {highlight} !important;\
     background-image: none !important;\
 }\
 li.ita-kd-menuitem-hover {\
-    background-color: #222222 !important;\
-    color: #222 !important;\
+    background-color: {bg_medium} !important;\
+    color: {bg_medium} !important;\
 }\
 div.ita-hwt-buttons, div.ita-hwt-candidates {\
-    border-top: 1px solid #777777 !important;\
+    border-top: 1px solid {light} !important;\
 }\
 div.ita-hwt-buttons {\
-    background-color: #222222 !important;\
-    background: #222222 !important;\
+    background-color: {bg_medium} !important;\
+    background: {bg_medium} !important;\
 }\
 ul.ita-kd-dropdown-menu {\
-    background-color: #333333 !important;\
+    background-color: {modals} !important;\
 }\
 div.ita-hwt-ime, div.ita-hwt-candidates {\
-    background-color: #696969;\
+    background-color: {bg_light};\
 }\
 div.ita-hwt-close {\
-    background-color: #393939 !important;\
+    background-color: {text} !important;\
 }\
 div.ita-hwt-grip {\
     opacity: 0.1;\
 }\
 div.ita-hwt-grip:hover, div.ita-hwt-close:hover {\
-    background-color: #696969 !important;\
+    background-color: {bg_light} !important;\
     opacity: 0.5;\
 }\
 div.ita-hwt-ime-st {\
-    background-color: #393939 !important;\
-    border-color: #777777;\
+    background-color: {text} !important;\
+    border-color: {light};\
 }\
 button.vk-btn {\
-    color: #393939;\
+    color: {text};\
     background-image: none !important;\
-    background-color: #696969 !important;\
-    border-color: #696969;\
+    background-color: {bg_light} !important;\
+    border-color: {bg_light};\
 }\
 button.vk-sf-h {\
-    background-color: #f1f1f1 !important;\
+    background-color: {highlight} !important;\
     background-image: none !important;\
 }\
 div.vk-box {\
-    background-color: #333333 !important;\
+    background-color: {modals} !important;\
 }\
 \
 /*APPS*/\
 div#gbwa > div+div > ul {\
-    background-color: #333333 !important;\
+    background-color: {modals} !important;\
 }\
 div#gbwa > div+div, div#gbwa > div+div > a {\
-    background-color: #222222 !important;\
-    background: #222222 !important;\
+    background-color: {bg_medium} !important;\
+    background: {bg_medium} !important;\
 }\
 div#gbwa > div+div > span {\
-    border-bottom: 1px solid #777777 !important;\
+    border-bottom: 1px solid {light} !important;\
 }\
 div.MNn0h {\
-    background-color: #222222 !important;\
-    background: #222222 !important;\
+    background-color: {bg_medium} !important;\
+    background: {bg_medium} !important;\
 }\
 .gb_Z:hover:not(.gb_0) a {\
-    border: 1px solid #222222 !important;\
+    border: 1px solid {bg_medium} !important;\
 }\
 .gb_Z:hover .gb_4 {\
-    background: #222222 !important;\
+    background: {bg_medium} !important;\
 }\
 \
 /*NOTIFICATIONS*/\
@@ -283,45 +292,45 @@ body.Vzc {\
 }\
 div#gbsfw {\
     border-color: transparent !important;\
-    background-color: #333333 !important;\
+    background-color: {modals} !important;\
 }\
 div.aac {\
-    background-color: #333333 !important;\
+    background-color: {modals} !important;\
 }\
 div.Kza {\
-    background-color: #555555 !important;\
-    background: #555555 !important;\
+    background-color: {text} !important;\
+    background: {text} !important;\
 }\
 div.Kza:after, div.Kza:before {\
-    border-top-color: #666666;\
+    border-top-color: {text};\
 }\
 \
 /*USERPANEL*/\
 div#gb > div > div > div+div > div+div+div+div > div+div > div > div > div {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 div.gb_R {\
-    background-color: #555555 !important;\
-    background: #555555 !important;\
+    background-color: {text} !important;\
+    background: {text} !important;\
 }\
-div#gb > div >div {\
-    background-color: #111111 !important;\
+div#gb > div > div {\
+    background-color: {bg_black} !important;\
     border: 0;\
 }\
 div#gb > div > div > div+div > div+div+div+div > div+div > div > div > a {\
-    background:#696969 !important;\
-    border-color: #666666 !important;\
-    color: #444444 !important;\
+    background: {bg_light} !important;\
+    border-color: {bg_light} !important;\
+    color: {text} !important;\
 }\
 div#gb > div > div > div+div > div+div+div+div > div+div > div > div\
  > a:hover {\
-    background-color: #f1f1f1 !important;\
+    background-color: {highlight} !important;\
     background-image: none !important;\
 }\
 div#gb > div > div > div+div > div+div+div+div > div+div,\
 div#gb > div > div > div+div > div+div+div+div > div+div > div+div+div {\
-    background-color: #222222 !important;\
-    background: #222222 !important;\
+    background-color: {bg_medium} !important;\
+    background: {bg_medium} !important;\
 }\
 div#gb > div > div > div+div > div+div+div+div > div+div > div > a:focus {\
     outline: none;\
@@ -329,32 +338,32 @@ div#gb > div > div > div+div > div+div+div+div > div+div > div > a:focus {\
 \
 /*SHARE*/\
 div.jfk-bubble {\
-    background-color: #393939;\
-    border-color: #777777 !important;\
+    background-color: {text};\
+    border-color: {light} !important;\
 }\
 div.share-panel{\
     background: none;\
 }\
 div.share-panel>h3 {\
-    color: #777777 !important;\
+    color: {light} !important;\
 }\
 \
 /*ARROWS*/\
 div.cd-exp-ar {\
-    background-color: #696969;\
+    background-color: {bg_light};\
 }\
 div.cd-exp-ar:after {\
-    border-top: 16px solid #222222;\
+    border-top: 16px solid {bg_medium};\
 }\
 div.cd-exp-ar:before {\
-    border-top:16px solid #696969;\
+    border-top:16px solid {bg_light};\
 }\
 div.jfk-bubble-arrowimplafter {\
-    border-color: #777777 transparent !important;\
+    border-color: {light} transparent !important;\
 }\
 div#gb > div > div > div+div > div+div+div+div > div > div+div,\
 div#gb > div > div > div+div > div+div > div > a+div+div {\
-    border-bottom-color: #333333 !important;\
+    border-bottom-color: {modals} !important;\
 }\
 \
 /*Miscellaneous*/\
@@ -367,13 +376,24 @@ div#t-new-user {\
     display: none;\
 }\
 div.gt-baf-sep {\
-    border-bottom: 1px solid #777777 !important;\
+    border-bottom: 1px solid {light} !important;\
+}\
+input:focus {\
+    border: none !important;\
 }\
 img#kwu_av:hover {\
     opacity: 1 !important;\
 }\
 \
 ";
+css = css.replace(new RegExp('{bg_black}', 'gi'), bg_black);
+css = css.replace(new RegExp('{bg_dark}', 'gi'), bg_dark);
+css = css.replace(new RegExp('{bg_medium}', 'gi'), bg_medium);
+css = css.replace(new RegExp('{modals}', 'gi'), modals);
+css = css.replace(new RegExp('{text}', 'gi'), text);
+css = css.replace(new RegExp('{bg_light}', 'gi'), bg_light);
+css = css.replace(new RegExp('{light}', 'gi'), light);
+css = css.replace(new RegExp('{highlight}', 'gi'), highlight);
 GM_addStyle(css);
 var panel=document.getElementById('gt-lang-right');
 var av='<a href="https://github.com/KeyWeeUsr/Userscripts"><img id="kwu_av"\
