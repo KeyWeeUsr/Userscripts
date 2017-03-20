9c76bb6dc199df12a4a215e70f5ad7c64cdee8eb
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-03-20 12:05:34 +0100

    CopyCode - Fix glitch in GH editor, bug with recursive DIVs, styling issues

--- CopyCode/CopyCode.user.js
+++ CopyCode/CopyCode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub CopyCode
 // @namespace    https://github.com/KeyWeeUsr/Userscripts
-// @version      0.3
+// @version      0.4
 // @description  Copy content of a code-block everywhere on GitHub
 // @author       Peter Badida
 // @copyright    2016+, Peter Badida
@@ -35,28 +35,61 @@
 
     function addButtons() {
         $("pre").each(function (i, codeBlock) {
-            var codeContainer = $("<div></div>");
             var id = "codeButton-" + i;
+            var parentId = "codeButton-parent-" + i;
+            var codeContainer = $('<div id="' + parentId + '"></div>');
+
+            // ignore PRE if it's a GH online editor
+            if (~String(codeBlock.getAttribute("class")).indexOf("CodeMirror")) {
+                return;
+            }
+
+            // ignore PRE if it already has a button
+            var oldId = codeBlock.parentNode.getAttribute("id");
+            if (~String(oldId).indexOf("-parent-")) {
+                return;
+            }
+
+            // put PRE block into DIV for easier adding of COPY button
             $(codeBlock).replaceWith(codeContainer);
             codeContainer.append(codeBlock);
 
+            // mark the container, so that it's ignored later
+            codeContainer.attr("id", parentId);
+
+            // create COPY button on mouse enter
             codeContainer.mouseenter(function () {
                 var codeButton = $('<div style="background-color: #000; ' +
                                    'color: #fff; cursor: pointer; display:' +
                                    'inline; font-size: 12pt; opacity: 0.5;' +
                                    'padding: 3px; position: absolute;">Copy' +
                                    '</div>');
+
+                // give button an ID for easier removing later
                 codeButton.attr("id", id);
+
                 codeContainer.append(codeButton);
                 codeButton.css('top', codeContainer.position().top);
-                codeButton.css('left', codeContainer.position().left +
+                codeButton.css('left',
+                               codeContainer.position().left +
                                codeContainer.width() - 50);
+
+                // select the whole text in PRE and copy to the clipboard
                 codeButton.click(function () {
                     selectText(codeBlock);
                     document.execCommand("copy");
                 });
+
+                // hovering styles
+                codeButton.mouseover(function () {
+                    codeButton.css('opacity', 1.0);
+                });
+                codeButton.mouseleave(function () {
+                    codeButton.css('opacity', 0.5);
+                });
             });
 
+            // remove COPY button on mouse leave
             codeContainer.mouseleave(function () {
                 $("#" + id).remove();
             });
@@ -64,10 +97,14 @@
     }
 
     function appendMe() {
-        var av=$('<div style="float: right;"><a href="https://github.com/Key' +
-                'WeeUsr/Userscripts"><img id="kwu_av" style="opacity: 0.3;" ' +
-                'src="https://github.com/identicons/KeyWeeUsr.png" width=' +
-                '"24"></img></a></div>');
+        var av_style = "opacity: 0.3; border-radius: 10px;"
+        var av_cont_style = "margin: 2px 10px -2px 0px;"
+        var av = $('<li style="' + av_cont_style + '">' +
+                   '<div style="float: right;"><a href="' +
+                   'https://github.com/KeyWeeUsr/Userscripts">' +
+                   '<img id="kwu_av" style="' + av_style + '" ' +
+                   'src="https://github.com/identicons/KeyWeeUsr.png" ' +
+                   'width="24"></img></a></div></li>');
 
         av.mouseenter(function () {
             $('#kwu_av').css('opacity', 1.0);
@@ -76,16 +113,15 @@
         av.mouseleave(function () {
             $('#kwu_av').css('opacity', 0.3);
         });
-        $('.gh-header-actions').append(av);
+        $('.pagehead-actions').prepend(av);
     }
 
     function run() {
+        addButtons();
         if (!document.getElementById('kwu_av')){
-            addButtons();
             appendMe();
-        } else {
-            clearInterval(runCheck);
         }
     }
+    // repeat in case of switching e.g. GH tabs
     var runCheck = setInterval(run, 250)
 })();
